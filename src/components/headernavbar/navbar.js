8b320e9d2c5bc7218e854a975bdf7a42bc2974a0
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-17 13:10:56 -0500

    connected header and navbar components to redux store to get links

--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Navbar extends Component {
-   render() {
-       return (
-           <div className="navbar">
-               
-           </div>
-       );
-   };
+    render() {
+        return (
+            <div className="navbar">
+                {
+                    this.props.navbarLinks.map((link, index) => {
+                        return(
+                            <a className="navbar-link" key={index} onClick={() => console.log("switch")}>
+                                {link.title}
+                            </a>
+                        )
+                    })
+                }
+            </div>
+        );
+    };
 }
+
+function mapStateToProps(state) {
+    const { navbarLinks } = state.headerNavbar;
+    return {
+        navbarLinks
+    }
+}
+
+Navbar = connect(mapStateToProps)(Navbar);
 
 export default Navbar;
