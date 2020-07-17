6db49bf0ecedcc1cc58c871cfc967df53d7efe8b
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-17 18:19:16 -0500

    built base structure for account components

---
+++ src/components/account/accountInformation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AccountInformation extends Component {
+   render() {
+       return (
+           <div>
+               Account
+           </div>
+       );
+   };
+}
+
+export default AccountInformation;
--- src/components/headernavbar/navbar.js
+++ src/components/headernavbar/navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component {
                 {
                     this.props.navbarLinks.map((link, index) => {
                         return(
-                            <a className="navbar-link" key={index} onClick={() => console.log("switch")}>
+                            <a className={`navbar-link ${link.active ? 'green-text' : ''}`} key={index} onClick={() => console.log('trying to switch tab')}>
                                 {link.title}
                             </a>
                         )
