813450b19d8a3966379654665442075443015e13
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-17 19:42:13 -0500

    setup purchase history component

--- src/components/pageTitle.js
+++ src/components/pageTitle.js
@@ -4,11 +4,9 @@ class PageTitle extends Component {
     render() {
         const { className, title } = this.props;
         return (
-            <div className={`${className} page-title`}>
-                {title}
-            </div>
-        );
-    };
+            <div className={`${className} page-title`}>{title}</div>
+        )
+    }
 }
 
 export default PageTitle;
