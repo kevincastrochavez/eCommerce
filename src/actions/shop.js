65b47c62579131b582187883bc3a98a6121ce7c6
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-20 18:31:06 -0500

    fetch products action

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -1,6 +1,7 @@
 import {
     // SET_SHOP_CATEGORIES
-    SET_NAVBAR_LINKS    
+    SET_NAVBAR_LINKS,
+    SET_SHOP_PRODUCTS   
 } from './types';
 
 export function fetchShopCategories() {
@@ -34,6 +35,63 @@ export function fetchShopCategories() {
             {
                 _id: 6,
                 title: 'Ruby'
+            }
+        ]
+    })
+}
+
+export function fetchShopProducts() {
+    return ({
+        type: SET_SHOP_PRODUCTS,
+        payload: [
+            {
+                _id: 0,
+                title: 'JavaScript in the Browser',
+                description: 'kasmkdaksmdion askndkanoksnf asndkasmkldn askndkans',
+                price: 1.99,
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 1,
+                title: 'Database',
+                description: 'kasmkdaksmdion askndkanoksnf asndkasmkldn askndkans',
+                price: 1.99,
+                belongsTo: [0, 3]
+            },
+            {
+                _id: 2,
+                title: 'Full Stack Development',
+                description: 'kasmkdaksmdion askndkanoksnf asndkasmkldn askndkans',
+                price: 1.99,
+                belongsTo: [0, 1, 4]
+            },
+            {
+                _id: 3,
+                title: 'UI Design',
+                description: 'kasmkdaksmdion askndkanoksnf asndkasmkldn askndkans',
+                price: 1.99,
+                belongsTo: [0, 2]
+            },
+            {
+                _id: 4,
+                title: 'JavaScript',
+                description: 'kasmkdaksmdion askndkanoksnf asndkasmkldn askndkans',
+                price: 1.99,
+                belongsTo: [0, 1]
+            },
+            {
+                _id: 5,
+                title: 'Advanced programming',
+                description: 'kasmkdaksmdion askndkanoksnf asndkasmkldn askndkans',
+                price: 1.99,
+                belongsTo: [0, 5]
+            },
+            {
+                _id: 6,
+                title: 'JavaScript Development',
+                description: 'kasmkdaksmdion askndkanoksnf asndkasmkldn askndkans',
+                price: 1.99,
+                belongsTo: [0, 1]
             }
         ]
     })
