51d8c6dda5103ffca8beafb97683db23c094044f
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-21 17:13:14 -0500

    built front of card and implemented rotation/flip animation ability

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -8,20 +8,29 @@ class ShopProduct extends Component {
         return (
             <div className='shop-product'>
 
-                <div className='shop-product-title'>
-                    {title}
+                <div className="shop-product-front">
+                    <img className="shop-product-front-img" src="http://via.placeholder.com/220x220" />
+                    <div className="shop-product-front-title">
+                        {title}
+                    </div>
                 </div>
+                
+                <div className="shop-product-back">
+                    <div className='shop-product-back-title'>
+                        {title}
+                    </div>
 
-                <div className='shop-product-description'>
-                    {description}
-                </div>
+                    <div className='shop-product-back-description'>
+                        {description}
+                    </div>
 
-                <GreenPriceTag className="shop-product-price" title={price} />
+                    <GreenPriceTag className="shop-product-back-price" title={price} />
 
-                <Quantity className="shop-product-quantity" quantity={1}/>
+                    <Quantity className="shop-product-back-quantity" quantity={1}/>
 
-                <div className="shop-product-add-to-cart">
-                    Add to Cart
+                    <div className="shop-product-back-add-to-cart">
+                        Add to Cart
+                    </div>
                 </div>
 
             </div>
