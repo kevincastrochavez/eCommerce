b98a9a5cd1306590d3e60269f33c2d54ab07a9a4
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-24 11:54:26 -0500

    created shop-cart component and began to style it

---
+++ src/components/shop/shopCart.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+function CartButton({className, icon}) {
+    return (
+        <div className={`${className} cart-button`}>
+            <i className={icon} />
+        </div>
+    )
+}
+
+function CartContent({ className}) {
+    return (
+        <div className={`${className} cart-content`}>
+
+        </div>
+    )
+}
+
+class ShopCart extends Component {
+    render() {
+        const { className } = this.props;
+
+        return (
+            <div className={`${className} shop-cart`}>
+                <CartButton className="shop-cart-toggle" icon="fas fa-times" />
+                <CartContent className="shop-cart-content" />
+            </div>
+        );
+    };
+}
+
+export default ShopCart;
