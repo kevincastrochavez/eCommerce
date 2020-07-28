2bb28b5048ef556c1455c69bf39556cfdfc0640d
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-07-27 20:11:55 -0500

    fixed create account button pushing to shop component when click

--- src/components/auth/signInForm.js
+++ src/components/auth/signInForm.js
@@ -29,25 +29,34 @@ class SignInForm extends Component {
         ]
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
+               
                 <Field className='sign-in-form__email'
-                type='email'
-                title='Email'
-                placeholder='Email'
-                name='email'
-                component={FormInput}/>
+                    type='email'
+                    title='Email'
+                    placeholder='Email'
+                    name='email'
+                    component={FormInput}
+                />
+
                 <Field className='sign-in-form__password'
-                type='password'
-                title='Password'
-                placeholder='Password'
-                name='password'
-                component={FormInput}/>
+                    type='password'
+                    title='Password'
+                    placeholder='Password'
+                    name='password'
+                    component={FormInput}
+                />
+
                 <div className='sign-in-form__line'></div>
+
                 <Field className='sign-in-form__login'
-                type='submit'
-                title='Login'
-                name='login'
-                component={FormButton}/>
+                    type='submit'
+                    title='Login'
+                    name='login'
+                    component={FormButton}
+                />
+                
                 <Details className='sign-in-form__details' title='QuickLinks' links={links}/>
+                
             </form>
         )
     }
--- src/components/auth/signUpForm.js
+++ src/components/auth/signUpForm.js
@@ -26,46 +26,67 @@ class SignUpForm extends Component {
         ]
         return (
             <form onSubmit={handleSubmit} className={`${className} sign-up-form`}>
-                <Field className='sign-up-form__name'
-                type='name'
-                title='Name'
-                placeholder='Name'
-                name='name'
-                component={FormInput}/>
-                <Field className='sign-up-form__email'
-                type='email'
-                title='Email'
-                placeholder='Email'
-                name='email'
-                component={FormInput}/>
-                <Field className='sign-up-form__password'
-                type='password'
-                title='Password'
-                placeholder='Password'
-                name='password'
-                component={FormInput}/>
-                <Field className='sign-up-form__confirm'
-                type='password'
-                title='Confirm Password'
-                placeholder='Confirm Password'
-                name='confirm'
-                component={FormInput}/>
+               
+                <Field
+                    className='sign-up-form__name'
+                    type='name'
+                    title='Name'
+                    placeholder='Name'
+                    name='name'
+                    component={FormInput}
+                />
+
+                <Field
+                    className='sign-up-form__email'
+                    type='email'
+                    title='Email'
+                    placeholder='Email'
+                    name='email'
+                    component={FormInput}
+                />
+
+                <Field
+                    className='sign-up-form__password'
+                    type='password'
+                    title='Password'
+                    placeholder='Password'
+                    name='password'
+                    component={FormInput}
+                />
+
+                <Field
+                    className='sign-up-form__confirm'
+                    type='password'
+                    title='Confirm Password'
+                    placeholder='Confirm Password'
+                    name='confirm'
+                    component={FormInput}
+                />
+
 
                 <div className='sign-up-form__line'></div>
-                <Field className='sign-up-form__login'
-                onClick={() => history.push('/account')}
-                type='submit'
-                title='Create Account'
-                name='login'
-                component={FormButton}/>
-                <Field className='sign-up-form__back'
-                onClick={() => history.push('/signin')}
-                type='button'
-                title='Back'
-                name='back'
-                short={true}
-                component={FormButton}/>
+
+                <Field
+                    className='sign-up-form__login'
+                    onClick={() => history.push('/shop')}
+                    type='submit'
+                    title='Create Account'
+                    name='login'
+                    component={FormButton}
+                />
+
+                <Field
+                    className='sign-up-form__back'
+                    onClick={() => history.push('/signin')}
+                    type='button'
+                    title='Back'
+                    name='back'
+                    short={true}
+                    component={FormButton}
+                />
+
                 <Details className='sign-up-form__details' title='Password Requirements' info={info}/>
+
             </form>
         )
     }
